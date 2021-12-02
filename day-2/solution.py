cc05a703cbad57a080acf81cbcccef6e9a06fcc3
stojan97 <stoki97.basketball@hotmail.com>
2021-12-02 12:23:41 +0100

    day2 solution

---
+++ day-2/solution.py
@@ -0,0 +1,35 @@
+def get_input():
+    with open('input') as f:
+        input = [tuple(line.strip().split()) for line in f]
+
+    return input
+
+
+move_func = [{
+    'forward': (lambda x, y, aim, u: (x + u, y, aim)),
+    'down': (lambda x, y, aim, u: (x, y + u, aim)),
+    'up': (lambda x, y, aim, u: (x, y - u, aim))
+}, {
+
+    'forward': (lambda x, y, aim, u: (x + u, y + (aim * u), aim)),
+    'down': (lambda x, y, aim, u: (x, y, aim + u)),
+    'up': (lambda x, y, aim, u: (x, y, aim - u))
+}
+]
+
+
+def solve_part(input, part):
+    x = y = 0
+    aim = 0
+
+    for dir, units in input:
+        u = int(units)
+        move = move_func[part][dir]
+        x, y, aim = move(x, y, aim, u)
+
+    return x * y
+
+
+input = get_input()
+print(solve_part(input, 0))
+print(solve_part(input, 1))
