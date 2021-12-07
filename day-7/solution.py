5f241a1c1583e88e27bd7a667d71eeef27307904
stojan97 <stoki97.basketball@hotmail.com>
2021-12-07 15:41:08 +0100

    day7 solution

--- day-7/solution.py
+++ day-7/solution.py
@@ -9,32 +9,21 @@ def get_input():
     return lines
 
 
-def part1(positions):
-    min_fuel = math.inf
-    limit = range(min(positions), max(positions) + 1)
-
-    for pos in limit:
-        fuel = sum(abs(pos - other_pos) for other_pos in positions)
-        min_fuel = min(min_fuel, fuel)
-
-    return min_fuel
-
-
 def s(n):
     return (n * (n + 1)) // 2
 
 
-def part2(positions):
+def solve(positions, calc):
     min_fuel = math.inf
     limit = range(min(positions), max(positions) + 1)
 
     for pos in limit:
-        fuel = sum(s(abs(pos - other_pos)) for other_pos in positions)
+        fuel = sum(calc(pos, other_pos) for other_pos in positions)
         min_fuel = min(min_fuel, fuel)
 
     return min_fuel
 
 
 input = get_input()
-print('Part 1:', part1(input))
-print('Part 2:', part2(input))
+print('Part 1:', solve(input, lambda pos, other_pos: abs(pos - other_pos)))
+print('Part 2:', solve(input, lambda pos, other_pos: s(abs(pos - other_pos))))
