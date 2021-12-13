f964e38efba2316cb771ba857a547fa0addff8b0
stojan97 <stoki97.basketball@hotmail.com>
2021-12-13 20:05:31 +0100

    day13 solution

--- day-13/solution.py
+++ day-13/solution.py
@@ -29,17 +29,15 @@ def fold_it(points, folds):
             for y, x in set(final_points):
                 if y > val:
                     next_y = val - (y - val)
-                    if next_y >= 0:
-                        final_points.remove((y, x))
-                        final_points.add((next_y, x))
+                    final_points.remove((y, x))
+                    final_points.add((next_y, x))
 
         if coord == 'x':
             for y, x in set(final_points):
                 if x > val:
                     next_x = val - (x - val)
-                    if next_x >= 0:
-                        final_points.remove((y, x))
-                        final_points.add((y, next_x))
+                    final_points.remove((y, x))
+                    final_points.add((y, next_x))
 
     return final_points
 
