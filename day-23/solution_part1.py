8b418d5460e813624e4342f043bf76133a1d9c72
stojan97 <stoki97.basketball@hotmail.com>
2022-01-04 17:46:16 +0100

    day23 solution

--- day-23/solution_part1.py
+++ day-23/solution_part1.py
@@ -6,38 +6,38 @@ from heapq import *
 
 class Amphipod:
 
-    def __init__(self, idx, type, home_nodes, energy) -> None:
+    def __init__(self, idx, type, home_locations, energy) -> None:
         self.idx = idx
         self.type = type
-        self.home_nodes = home_nodes
+        self.home_locations = home_locations
         self.energy = energy
 
     def __repr__(self) -> str:
-        return f'Amphipod (idx={self.idx}, type={self.type}, home_nodes={self.home_nodes}, energy={self.energy})'
+        return f'Amphipod (idx={self.idx}, type={self.type}, home_locations={self.home_locations}, energy={self.energy})'
 
-    def is_home(self, amphipods_location: dict):
-        return amphipods_location[self.idx] in self.home_nodes
+    def is_home(self, location_for: dict):
+        return location_for[self.idx] in self.home_locations
 
 
 class State:
 
-    def __init__(self, total_energy, amphipod, occupied_by) -> None:
+    def __init__(self, total_energy, amphipod, amphipod_for) -> None:
         self.total_energy = total_energy
         self.amphipod = amphipod
-        self.occupied_by = occupied_by
+        self.amphipod_for = amphipod_for
 
     def __lt__(self, other: 'State'):
         return self.total_energy < other.total_energy
 
     def __repr__(self) -> str:
-        return f'State (current_amphipod={self.amphipod}, occupied_by={self.occupied_by}'
+        return f'State (current_amphipod={self.amphipod}, amphipod_for={self.amphipod_for}'
 
     def __eq__(self, o: 'State') -> bool:
-        return self.amphipod == o.amphipod and self.occupied_by == o.occupied_by
+        return self.amphipod == o.amphipod and self.amphipod_for == o.amphipod_for
 
     def __hash__(self) -> int:
-        occupied_by_items = tuple(sorted(self.occupied_by.items()))
-        return hash((self.amphipod, occupied_by_items))
+        amphipod_for_items = tuple(sorted(self.amphipod_for.items()))
+        return hash((self.amphipod, amphipod_for_items))
 
 
 def add_edge(first, second, nodes):
@@ -45,8 +45,8 @@ def add_edge(first, second, nodes):
     nodes[second].append(first)
 
 
-def invert_dict(occupied_by: dict):
-    return {v: k for k, v in occupied_by.items()}
+def invert_dict(amphipod_for: dict):
+    return {v: k for k, v in amphipod_for.items()}
 
 
 def get_input():
@@ -99,32 +99,32 @@ def get_input():
     return amphipods, nodes
 
 
-def all_covered(amphipods, amphipods_location):
-    return all(amphipod.is_home(amphipods_location) for amphipod in amphipods)
+def all_covered(amphipods, location_for):
+    return all(amphipod.is_home(location_for) for amphipod in amphipods)
 
 
-def get_covered(amphipods, amphipods_location):
-    return sum(amphipod.is_home(amphipods_location) for amphipod in amphipods)
+def get_covered(amphipods, location_for):
+    return sum(amphipod.is_home(location_for) for amphipod in amphipods)
 
 
-def are_all_stacked(amphipods, start, limit, occupied_by, add, amphipods_location):
+def are_all_stacked(amphipods, start, limit, amphipod_for, add, location_for):
     if start == limit:
         return True
 
-    return all(node in occupied_by and amphipods[occupied_by[node]].is_home(amphipods_location) for node in range(start, limit + add))
+    return all(location in amphipod_for and amphipods[amphipod_for[location]].is_home(location_for) for location in range(start, limit + add))
 
 
 SKIP_ROOMS = {10, 12, 14, 16} | {i for i in range(8)}
 TOP_ROOMS = {8, 9, 11, 13, 15, 17, 18}
 
 
-def get_possible_next_nodes(nodes, amphipod_idx, occupied_by, amphipods, amphipods_location):
-    start_location = amphipods_location[amphipod_idx]
-    home_nodes = amphipods[amphipod_idx].home_nodes
+def get_possible_next_nodes(nodes, amphipod_idx, amphipod_for, amphipods, location_for):
+    start_location = location_for[amphipod_idx]
+    home_locations = amphipods[amphipod_idx].home_locations
     amphipod_energy = amphipods[amphipod_idx].energy
-    h_nodes_set = set(home_nodes)
+    home_locations_set = set(home_locations)
 
-    stacked_same_kind = start_location in h_nodes_set and are_all_stacked(amphipods, home_nodes[0], start_location, occupied_by, 1, amphipods_location)
+    stacked_same_kind = start_location in home_locations_set and are_all_stacked(amphipods, home_locations[0], start_location, amphipod_for, 1, location_for)
 
     if stacked_same_kind:
         return {}
@@ -132,9 +132,9 @@ def get_possible_next_nodes(nodes, amphipod_idx, occupied_by, amphipods, amphipo
     dist = {}
     q = deque([(start_location, 0)])
 
-    visited = set(occupied_by.keys())
+    visited = set(amphipod_for.keys())
     to_skip = set(SKIP_ROOMS)
-    to_skip.difference_update(h_nodes_set)
+    to_skip.difference_update(home_locations_set)
 
     while q:
         next_node, energy = q.popleft()
@@ -147,16 +147,16 @@ def get_possible_next_nodes(nodes, amphipod_idx, occupied_by, amphipods, amphipo
                 visited.add(node)
                 q.append((node, energy_so_far))
 
-    first_home_node = next(filter(lambda n: n in dist, home_nodes), None)
+    first_home_node = next(filter(lambda n: n in dist, home_locations), None)
 
-    if first_home_node is not None and are_all_stacked(amphipods, home_nodes[0], first_home_node, occupied_by, 0, amphipods_location):
+    if first_home_node is not None and are_all_stacked(amphipods, home_locations[0], first_home_node, amphipod_for, 0, location_for):
         return {first_home_node: dist[first_home_node]}
 
     # amphipod was locked
     if start_location in TOP_ROOMS:
         return {}
 
-    for home_node in home_nodes:
+    for home_node in home_locations:
         if home_node in dist:
             dist.pop(home_node)
 
@@ -169,10 +169,10 @@ def solve(input):
     print(*nodes, sep='\n')
     pq = []
     heapify(pq)
-    occupied_by = {i: i for i in range(8)}
+    amphipod_for = {i: i for i in range(8)}
 
     for i in range(1, 8, 2):
-        st = (State(0, i, copy(occupied_by)))
+        st = (State(0, i, copy(amphipod_for)))
         heappush(pq, st)
 
     c = 0
@@ -183,36 +183,36 @@ def solve(input):
         state = heappop(pq)
         total_energy = state.total_energy
         amphipod_idx = state.amphipod
-        current_occupied_by = state.occupied_by
-        amphipods_location = invert_dict(current_occupied_by)
-        amphipod_location = amphipods_location[amphipod_idx]
+        current_amphipod_for = state.amphipod_for
+        location_for = invert_dict(current_amphipod_for)
+        amphipod_location = location_for[amphipod_idx]
 
         if state in memo:
             continue
 
         memo.add(state)
 
-        covered = get_covered(amphipods, amphipods_location)
+        covered = get_covered(amphipods, location_for)
         if c % 10000 == 0:
             print(f'Covered: {covered}, energy={total_energy}, count={c}')
 
         c += 1
 
-        if all_covered(amphipods, amphipods_location):
+        if all_covered(amphipods, location_for):
             print(f'Covered: {covered}, energy={total_energy}, count={c}')
             return total_energy
 
-        possible_next_nodes = get_possible_next_nodes(nodes, amphipod_idx, current_occupied_by, amphipods, amphipods_location)
+        possible_next_nodes = get_possible_next_nodes(nodes, amphipod_idx, current_amphipod_for, amphipods, location_for)
 
-        for next_node, energy in possible_next_nodes.items():
+        for next_location, energy in possible_next_nodes.items():
             next_energy = total_energy + energy
-            next_occupied_by = copy(current_occupied_by)
-            next_occupied_by.pop(amphipod_location)
-            next_occupied_by[next_node] = amphipod_idx
+            next_amphipod_for = copy(current_amphipod_for)
+            next_amphipod_for.pop(amphipod_location)
+            next_amphipod_for[next_location] = amphipod_idx
 
-            for idx, _ in enumerate(amphipods):
-                if idx != amphipod_idx:
-                    new_state = State(next_energy, idx, next_occupied_by)
+            for next_amphipod_idx, _ in enumerate(amphipods):
+                if next_amphipod_idx != amphipod_idx:
+                    new_state = State(next_energy, next_amphipod_idx, next_amphipod_for)
                     heappush(pq, new_state)
 
     return -1
