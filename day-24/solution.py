d76bea1042c209c3e7de73d79418fc66af1b91e7
stojan97 <stoki97.basketball@hotmail.com>
2022-01-10 10:27:24 +0100

    day24 solution

--- day-24/solution.py
+++ day-24/solution.py
@@ -29,51 +29,51 @@ def get_input():
     for i in range(len(groups)):
         reduced_instructions.append((int(groups[i][-3][2]), int(groups[i][4][2]), ('div', 'z', '26') in groups[i]))
 
-    bi_graph = [0] * len(reduced_instructions)
+    matching_pair = [0] * len(reduced_instructions)
     stack = deque()
 
     for idx, reduced_instruction in enumerate(reduced_instructions):
         if reduced_instruction[2]:
             pop = stack.pop()
-            bi_graph[pop] = idx
-            bi_graph[idx] = pop
+            matching_pair[pop] = idx
+            matching_pair[idx] = pop
         else:
             stack.append(idx)
 
-    return reduced_instructions, bi_graph
+    return reduced_instructions, matching_pair
 
 
-def find_input(bi_graph, input, instructions, minimize):
+def find_input(matching_pair, input, instructions, minimize):
     for i in range(len(instructions)):
         _, subtract_const, inverse_instruction = instructions[i]
         if inverse_instruction:
             # find the matching instruction
-            add_const, _, _ = instructions[bi_graph[i]]
+            add_const, _, _ = instructions[matching_pair[i]]
             diff = add_const + subtract_const
 
             if diff < 0:
                 if minimize:
-                    input[bi_graph[i]] += abs(diff)
+                    input[matching_pair[i]] += abs(diff)
                 else:
                     input[i] -= abs(diff)
             else:
                 if minimize:
                     input[i] += abs(diff)
                 else:
-                    input[bi_graph[i]] -= abs(diff)
+                    input[matching_pair[i]] -= abs(diff)
 
     return ''.join(map(str, input))
 
 
-def solve(instructions, bi_graph):
+def solve(instructions, matching_pair):
     # We can construct bi-graph from the input pairs which are dependant on each other
-    maximized = find_input(bi_graph, [9] * len(instructions), instructions, False)
-    minimized = find_input(bi_graph, [1] * len(instructions), instructions, True)
+    maximized = find_input(matching_pair, [9] * len(instructions), instructions, False)
+    minimized = find_input(matching_pair, [1] * len(instructions), instructions, True)
 
     return maximized, minimized
 
 
-instructions, b_graph = get_input()
-part1, part2 = solve(instructions, b_graph)
+instructions, matching_pair = get_input()
+part1, part2 = solve(instructions, matching_pair)
 print('Part 1:', part1)
 print('Part 2:', part2)
